c82d5cbf8b3c4a78349d9fb33250958271a45a57
Asid-Ashi <asidashi98@gmail.com>
2021-06-30 14:19:40 +0530

    imported array

--- src/search.js
+++ src/search.js
@@ -1,3 +1,3 @@
 
-export const employee =[{name:"asid" , id:47},{name:"basl" , id:66},{name:"ash" , id:55},{name:"mirsh" , id:36}]
+export const employee =[{name:"asid" , id:47},{name:"basl" , id:66},{name:"ash" , id:55},{name:"mirsh" , id:36},{name:"jhon" , id:47},{name:"shyma" , id:66},{name:"nidhi" , id:85},{name:"anju" , id:33}]
 
