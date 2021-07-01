60907e3e10322854ad5194371719f45b81e47a0d
Asid-Ashi <asidashi98@gmail.com>
2021-07-01 15:35:56 +0530

    api implemented

--- src/App.js
+++ src/App.js
@@ -1,45 +1,67 @@
 import './App.css';
-import { useState ,useEffect} from 'react';
-import { employee } from './search';
+import { useState, useEffect, useCallback } from 'react';
+//import { employee } from './search';
 
-const App=()=> {
+import axios from 'axios';
+
+const App = () => {
   const [text, setText] = useState("")
-  const [array, setarray] = useState(employee)
-  
+  const [array, setarray] = useState([])
+  const [responseData, setResponseData] = useState([]);
 
- 
+  const fetchData = () => {
+    axios({
+      "method": "GET",
+      "url": "https://60dc1e14c2b6280017feb6c2.mockapi.io/react/users",
+
+    })
+      .then((response) => {
+        setResponseData(response.data)
+        setarray(response.data)
+      })
+  }
+
+
   useEffect(() => {
-    if(text.length >= 3){
+    fetchData()
+  }, [])
+
+
+  useEffect(() => {
+    if (text.length >= 3) {
       setarray(searchName(text))
     }
-    if(text.length === 0){
-      setarray(employee)
+    if (text.length === 0) {
+      setarray(responseData)
     }
   }, [text])
 
   const searchName = (queryString) => {
     if (queryString === '') {
-      return employee;
+      return responseData;
     }
     const regex = new RegExp(`${queryString.trim()}`, 'i');
-    return employee.filter(
-      (array) => array.name.search(regex) >= 0
-    );
+    return responseData.filter(
+      (array =>array.name.search(regex) >= 0
+    ));
   };
+
 
   return (
     <div className="App" >
-      <ul> 
-        Name of employee  </ul>
-        <input type="text" value={text} placeholder="search" onChange={(e)=>setText(e.target.value)}></input>
-        
-        {array.map((item)=>{
-          
-          return(
-          <li >{item.name}</li>)
+      <ul>Name of employee</ul>
+      <input type="text" value={text} placeholder="search" onChange={(e) => setText(e.target.value)}></input>
 
-        })}
-       
+      {array && array.map((item) => {
+
+        return (
+          <li >{item.name}</li>
+          )
+
+      })}
+
+      {/* {responseData && JSON.stringify(responseData, null, 4)}  */}
+
     </div>
   );
 }
